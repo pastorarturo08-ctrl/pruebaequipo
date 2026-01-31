22365b5fe90c7d84f741cce7d335f0f24a3d559d
Pc16 <Pc16@Villa16>
2026-01-31 11:02:04 -0600

    codigoprincipal

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingrese el primer número:");
+        int a = sc.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = sc.nextInt();
+
+        //Suma suma = new Suma();
+        //System.out.println("Suma: " + suma.calculadora(a, b));
+    }
+    
+}
